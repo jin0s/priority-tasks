f3bfd06fb88afde9ab7d048fcd2a9a356c463a25
Tom G <thomas.guarnery@gmail.com>
2021-01-30 12:47:05 -0700

    feature: add business sort for tasks, includes tests

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,3 +1,30 @@
+import { TaskDto } from '../dtos/tasks.dto';
+
 export const isEmptyObject = (obj: any): boolean => {
   return !Object.keys(obj).length;
+};
+
+/*
+ * The method sorts an array of TaskDtos based on business logic
+ * First sort by computedWeight descending
+ *    If tied sort by userWeight descending
+ *      If tied sort by delta between ceiling and floor ascending
+ */
+export const sortTasks = (tasks: TaskDto[]): TaskDto[] => {
+  return tasks.sort((a, b) =>
+    // First sort by computedWeight descending
+    parseFloat(b.computedWeight) > parseFloat(a.computedWeight)
+      ? 1
+      : // If tied sort by userWeight descending
+      parseFloat(b.computedWeight) === parseFloat(a.computedWeight)
+      ? parseFloat(b.userWeight) > parseFloat(a.userWeight)
+        ? 1
+        : // If tied sort by delta between ceiling and floor ascending
+        parseFloat(b.userWeight) === parseFloat(a.userWeight)
+        ? b.repeatCeiling - b.repeatFloor < a.repeatCeiling - b.repeatFloor
+          ? 1
+          : -1
+        : -1
+      : -1,
+  );
 };
